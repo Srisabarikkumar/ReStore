625b38eea0352a80bb368b88e06d40b89c373e38
Srisabarikkumar <srisabarikkumar@gmail.com>
2024-03-19 15:43:50 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt => 
+builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -23,6 +24,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
@@ -30,13 +36,13 @@ app.MapControllers();
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-    
+
 try
 {
     context.Database.Migrate();
     DbInitializer.Initialize(context);
 }
-catch (Exception ex) 
+catch (Exception ex)
 {
     logger.LogError(ex, "A problem occured during migration");
 }
